7edd835b02f9e989c9d7fcf10afd3140064c9fff
Cesar Soto Alarcon <Cesar@cesars-mac.local>
2016-01-18 23:00:41 -0300

    Version 2

--- Tarea03.U3.Cesar.Soto/ViewController.swift
+++ Tarea03.U3.Cesar.Soto/ViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var txtTemp: UITextField!
+    @IBOutlet weak var lblConverted: UILabel!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +25,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func indexChanged(sender: AnyObject) {
+        var num :Float
+               let numberformatted=NSNumberFormatter()
+         let number=numberformatted.numberFromString(txtTemp.text!)
+         let numberfloat=number?.floatValue
 
+       if(segmentedControl.selectedSegmentIndex==0)
+        {
+            
+           num=((numberfloat)!-32)*0.55555555
+            lblConverted.text="\(num) °Celsius"
+    
+       }else if(segmentedControl.selectedSegmentIndex==1)
+       {
+          num=((numberfloat)!*1.8)+32
+          lblConverted.text="\(num) ° Fahrenheit"
+        
+        }
+    }
+   
+   
+
+   
+            
+       
+    
 }
 
